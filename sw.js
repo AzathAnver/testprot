47f2c15f6650cbe4fb74c85407706e2e3b187ca6
AzathAnver <115231691+AzathAnver@users.noreply.github.com>
2024-09-10 22:24:52 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: 'AIzaSyCwz3xWFV-PO1atYeoxyqEqPDjZwgkCxh4',
+    messagingSenderId: 'testkaushik-d342d.fi',
+    appId: '1:488519019681:web:ac42278cf4735a472e94e0',
+    projectId: 'testkaushik-d342d',
+    user_key: 'ADGMOT35CHFLVDHBJNIG50K96AFVJ0K9JQDP4GCKL24NBFB95S9G',
+    siteid: 'b794f163edb52d00e4a0ba7fc0d42e3b'
+};
+importScripts('//cdnt.netcoresmartech.com/swv4.js');
